d9bc295c244031d62d49b39d572ce6513ebbd84d
DFooRS <dima.lutsenko03@gmail.com>
2022-10-02 12:05:03 +0300

    Добавлено сообщение о завершении аннексии с названием региона. Добавлено название региона в сообщение о начале аннексии

--- BEBRA/BEBRA/Program.cs
+++ BEBRA/BEBRA/Program.cs
@@ -13,6 +13,9 @@ namespace BEBRA
             public string rname;
             public int id;
         }
+
+
+      
         static void Main(string[] args)
         {
             Region Crimea;
@@ -39,22 +42,40 @@ namespace BEBRA
             Hobl = new Region();
             Hobl.rname = "Херсонская область";
             Hobl.id = 4;
+           
+            string anneksname = "Name";
+            int id = 5;
+            while(id != Crimea.id && id != DNR.id && id != LNR.id && id != Zobl.id && id != Hobl.id)
+            {
+                Console.WriteLine("Выберите регион, который хотите аннексировать:\n");
+                Console.WriteLine("| Крым(0) | ДНР (1) | ЛНР(2) | Запорожье(3) | Херсонесс(4) |\n");
+                Console.WriteLine("*Примечание: введите цифру, обозначающую регион\n");
+                id = Convert.ToInt32(Console.ReadLine());
 
-            
-            Console.WriteLine("Выберите регион, который хотите аннексировать:\n");
-            Console.WriteLine("| Крым(0) | ДНР (1) | ЛНР(2) | Запорожье(3) | Херсонесс(4) |\n");
-            Console.WriteLine("*Примечание: введите цифру, обозначающую регион\n");
-            int id = Convert.ToInt32(Console.ReadLine());
-
+                if (id == Crimea.id)
+                    anneksname = Crimea.rname;
+                else if (id == DNR.id)
+                    anneksname = DNR.rname;
+                else if (id == LNR.id)
+                    anneksname = LNR.rname;
+                else if (id == Zobl.id)
+                    anneksname = Zobl.rname;
+                else if (id == Hobl.id)
+                    anneksname = Hobl.rname;
+                else
+                    Console.WriteLine("Ошибка.Введите корректный номер региона.\n");
+            }
 
             int status = 0;
-            Console.WriteLine("Начата аннексия выбранного региона");
+            Console.WriteLine("Начата аннексия региона {0}", anneksname);
             while (status < 100)
             {
                 Console.Write("Аннексия региона завершена на {0}", status);
                 Console.WriteLine("процентов.");
                 status += 10;
             }
+            Console.Write("Аннексия региона {0}", anneksname);
+            Console.Write(" завершена успешно.");
             Console.ReadKey();
         }
     }
