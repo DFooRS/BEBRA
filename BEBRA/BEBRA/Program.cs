cf1d09c78e9e1d64d2bbb68bbebebf4e141de354
DFooRS <dima.lutsenko03@gmail.com>
2022-10-02 11:35:12 +0300

    Добавлены объекты класса Регион, добавлен выбор региона

--- BEBRA/BEBRA/Program.cs
+++ BEBRA/BEBRA/Program.cs
@@ -15,6 +15,37 @@ namespace BEBRA
         }
         static void Main(string[] args)
         {
+            Region Crimea;
+            Crimea = new Region();
+            Crimea.rname = "Крым";
+            Crimea.id = 0;
+
+            Region DNR;
+            DNR = new Region();
+            DNR.rname = "ДНР";
+            DNR.id = 1;
+
+            Region LNR;
+            LNR = new Region();
+            LNR.rname = "ЛНР";
+            LNR.id = 2;
+            
+            Region Zobl;
+            Zobl = new Region();
+            Zobl.rname = "Запорожская область";
+            Zobl.id = 3;
+
+            Region Hobl;
+            Hobl = new Region();
+            Hobl.rname = "Херсонская область";
+            Hobl.id = 4;
+
+            
+            Console.WriteLine("Выберите регион, который хотите аннексировать:\n");
+            Console.WriteLine("| Крым(0) | ДНР (1) | ЛНР(2) | Запорожье(3) | Херсонесс(4) |\n");
+            Console.WriteLine("*Примечание: введите цифру, обозначающую регион\n");
+            int id = Convert.ToInt32(Console.ReadLine());
+
 
             int status = 0;
             Console.WriteLine("Начата аннексия выбранного региона");
