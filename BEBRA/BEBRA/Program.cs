c1e1b39f7558c6ffc8941a4e210a44cae809d641
DFooRS <dima.lutsenko03@gmail.com>
2022-10-02 11:07:01 +0300

    Создан проект, добавлен цикл о статусе аннексии

---
+++ BEBRA/BEBRA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEBRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int status = 0;
+            Console.WriteLine("Начата аннексия выбранного региона");
+            while (status < 100)
+            {
+                Console.Write("Аннексия региона завершена на {0}", status);
+                Console.WriteLine("процентов.");
+                status += 10;
+            }
+            Console.ReadKey();
+        }
+    }
+}
